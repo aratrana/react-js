610be46fc30d8dccfc5add376f052b7b4c6260ab
Arat Rana <arana2@opentext.com>
2023-12-08 12:15:28 +0530

    Add to favorite
    Add to favorite

--- react-course/src/index.js
+++ react-course/src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import FavoritesContext from "./store/favorites-context";
+import {FavoritesContextProvider} from "./store/favorites-context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <FavoritesContext>
+  <FavoritesContextProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  // </FavoritesContext>
+  </FavoritesContextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- react-course/src/pages/Favorites.js
+++ react-course/src/pages/Favorites.js
@@ -1,7 +1,15 @@
-function Favorite() {
-    return (
-        <div>All Favorite page</div>
-    )
-}
+import { useContext } from "react";
+import FavoritesContext from "../store/favorites-context";
+import MeetupList from "../components/meetups/MeetupList";
+
+const Favorite = () => {
+  const favoriteCtx = useContext(FavoritesContext);
+  return (
+    <section>
+      <h1>My Favorites</h1>
+      <MeetupList meetups={favoriteCtx.favorites} />
+    </section>
+  );
+};
 
 export default Favorite;
